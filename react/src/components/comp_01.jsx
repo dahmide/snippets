3b946387e8a4291cfeec15dc36a4c53c3d548bd0
dahmide <blarrklad@gmail.com>
2026-03-26 23:58:20 +0100

    chore: Further commit

--- react/src/components/comp_01.jsx
+++ react/src/components/comp_01.jsx
@@ -1,51 +1,40 @@
-import classes from "./";
-import { RiSearch2Line } from "react-icons/ri";
 import { Field, FieldInput, FieldLabel } from "@/components/ui/field/Field";
-import { Button } from "@/components/ui/button/Button";
-import clsx from "clsx";
 
-export function PopoverContent({ tokens }) {
-    return (
-        <>
-            <Field className={classes.header__popoverContentSearch}>
-                <FieldLabel
-                    className={classes.header__popoverContentSearchLabel}>
-                    <div className={classes.header__popoverContentSearchLabel}>
-                        <RiSearch2Line />
-                        <span className="sr-only">Search</span>
-                    </div>
+<>
+    <Field className={classes.header__popoverContentSearch}>
+        <FieldLabel className={classes.header__popoverContentSearchLabel}>
+            <div className={classes.header__popoverContentSearchLabel}>
+                <RiSearch2Line />
+                <span className="sr-only">Search</span>
+            </div>
 
-                    <Input
-                        id={id}
-                        type="search"
-                        placeholder="Search..."
-                        className={classes.header__popoverContentSearchLabel}
-                    />
-                    <Button
-                        variant="ghost"
-                        className={classes.header__popoverContentSearchLabel}>
-                        <RiSearch2Line />
-                        <span className="sr-only">Search</span>
-                    </Button>
-                </FieldLabel>
-            </Field>
+            <Input
+                id={id}
+                type="search"
+                placeholder="Search..."
+                className={classes.header__popoverContentSearchLabel}
+            />
+            <Button
+                variant="ghost"
+                className={classes.header__popoverContentSearchLabel}>
+                <RiSearch2Line />
+                <span className="sr-only">Search</span>
+            </Button>
+        </FieldLabel>
+    </Field>
 
-            {tokens.length > 0 ? (
-                <div className={classes.header__popoverContentTokens}>
-                    {tokens.map((token, index) => (
-                        <Button
-                            className={classes.header__popoverContentTokensItem}
-                            key={index}
-                            onClick={() => null}>
-                            //Hello
-                        </Button>
-                    ))}
-                </div>
-            ) : (
-                <div className={classes.header__popoverContentStatus}>
-                    isLoading...
-                </div>
-            )}
-        </>
-    );
-}
+    {tokens.length > 0 ? (
+        <div className={classes.header__popoverContentTokens}>
+            {tokens.map((token, index) => (
+                <Button
+                    className={classes.header__popoverContentTokensItem}
+                    key={index}
+                    onClick={() => null}>
+                    //Hello
+                </Button>
+            ))}
+        </div>
+    ) : (
+        <div className={classes.header__popoverContentStatus}>isLoading...</div>
+    )}
+</>;
